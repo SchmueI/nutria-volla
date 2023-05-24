a896e1e7cd1799bf2beb525cfe8fab588fc764a1
Schmuel <dev@schmuel.net>
2023-05-24 23:14:04 +0200

    Reset System App and seperate Homescreen to Volla Home

--- apps/system/components/lock_screen.js
+++ apps/system/components/lock_screen.js
@@ -251,8 +251,8 @@ class LockScreen extends HTMLElement {
         hour12: false,
       });
     }
-    this.time.textContent = this.timeFormat.format(now).replace(":", "\n");
-    
+    this.time.textContent = this.timeFormat.format(now);
+
     if (!this.dateFormat) {
       this.dateFormat = new Intl.DateTimeFormat("default", {
         weekday: "long",
@@ -261,7 +261,7 @@ class LockScreen extends HTMLElement {
         timeZone: "UTC",
       });
     }
-    this.date.textContent = this.dateFormat.format(now).replace(", ", ",\n");
+    this.date.textContent = this.dateFormat.format(now);
   }
 
   reset() {
